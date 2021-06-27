c5a5ed034abc364b1c256e75f5cd296d746f31bc
Maxim <4andromed4@gmail.com>
2021-06-27 11:35:28 +0200

    added retrieval of object info on get request

--- recommendations/recommendations/urls.py
+++ recommendations/recommendations/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from recommendations import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('data', include('get_objects.urls'))
-]
+    path('data/', include('get_objects.urls'))
+] 
